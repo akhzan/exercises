7ed1f26144d139e955d45621c4e80fabc7c580df
syafrizal <rizal.akhzan@gmail.com>
2017-11-28 12:04:05 +0700

    adding more tests

--- test/unit/specs/Weather.spec.js
+++ test/unit/specs/Weather.spec.js
@@ -2,26 +2,31 @@ import Vue from 'vue'
 import Weather from '@/components/Weather'
 
 describe('Weather.vue', () => {
+  const Constructor = Vue.extend(Weather)
+  const vm = new Constructor().$mount()
   it('should render jakarta at first value of dropdown', () => {
-    const Constructor = Vue.extend(Weather)
-    const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.city option:checked').textContent.trim().replace(`/n`,''))
     .to.equal('Jakarta')
   })
-  it('should render jakarta at first header of grid', () => {
-    const Constructor = Vue.extend(Weather)
-    const vm = new Constructor().$mount()
-    Vue.nextTick(() => {
-      expect(vm.$el.querySelector('th').textContent).toBe('Jakarta')
-      done()
-    })
+  it('should render 3 value at dropdown', () => {
+    setTimeout(() => {
+      expect(vm.$el.querySelectorAll('option').length).to.equal(3)
+    },1000)
   })
-  // it('should render correct contents', done => {
-  //   const Constructor = Vue.extend(Weather)
-  //   const vm = new Constructor().$mount()
-  //   Vue.nextTick(() => {
-  //     expect(vm.gridData.length).toBe(5)
-  //     done()
-  //   })
-  // })
+  it('should render jakarta at first header of grid', () => {
+    setTimeout(() => {
+      expect(vm.$el.querySelector('tr th').textContent).to.equal('Jakarta')
+    },1000)
+  })
+  it('should render 3 rows at grid header', () => {
+    setTimeout(() => {
+      expect(vm.$el.querySelectorAll('th').length)
+      .to.equal(3)
+    },1000)
+  })
+  it('gridData should have length = 5', () => {
+    setTimeout(() => {
+      expect(vm.gridData.length).to.equal(5)
+    },1000)
+  })
 })
